708b29934479afeca5ec96d57f5296679c4b1239
FelixTheWhale <dennis1945lee@gmail.com>
2025-06-02 23:56:36 +0200

    Update vite.config.ts
    Github Pages Basepath

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/helix/',  
+      base: '/helix-wallet/',  
       define: {
 
       },
